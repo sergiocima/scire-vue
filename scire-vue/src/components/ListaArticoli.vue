7cbcb0aebdddd8c93884ffd1124eda87a4ad4b3c
Sergio Cima <sergiocima@gmail.com>
2024-11-07 13:18:17 +0100

    Update ListaArticoli.vue

--- scire-vue/src/components/ListaArticoli.vue
+++ scire-vue/src/components/ListaArticoli.vue
@@ -56,7 +56,6 @@ export default {
 .articolo {
   background-color: #f5f5f5;
   border-radius: 12px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   overflow: hidden;
   transition: transform 0.3s ease, box-shadow 0.3s ease;
   text-decoration: none;
@@ -66,7 +65,7 @@ export default {
 
 .articolo:hover {
   transform: translateY(-5px);
-  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15);
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15); /* Ombra solo al passaggio del mouse */
 }
 
 .articolo img {
