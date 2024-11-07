8f5114c5af66607712d0307cae3d0009215d1d37
Sergio Cima <sergiocima@gmail.com>
2024-11-07 14:34:16 +0100

    Update vue.config.js

--- scire-vue/vue.config.js
+++ scire-vue/vue.config.js
@@ -2,6 +2,7 @@
 const webpack = require('webpack');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   configureWebpack: config => {
     config.plugins.push(
       new webpack.DefinePlugin({
