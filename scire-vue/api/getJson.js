8d7745c7f7833bfcd807caad492c63755f269921
Sergio Cima <sergiocima@gmail.com>
2024-11-07 08:42:51 +0100

    Update getJson.js

--- scire-vue/api/getJson.js
+++ scire-vue/api/getJson.js
@@ -3,7 +3,9 @@ import fetch from 'node-fetch';
 export default async (req, res) => {
   try {
     console.log("Inizio richiesta a scienzainrete");
-    const response = await fetch('https://www.scienzainrete.it/json');
+    const response = await fetch('https://www.scienzainrete.it/json', {
+      headers: { 'Content-Type': 'application/json' }
+    });
     console.log("Stato della risposta:", response.status);
 
     if (!response.ok) {
@@ -15,6 +17,6 @@ export default async (req, res) => {
     res.status(200).json(data);
   } catch (error) {
     console.error("Errore nella funzione serverless:", error);
-    res.status(500).json({ error: 'Errore nel recupero dei dati JSON' });
+    res.status(500).json({ error: 'Errore nel recupero dei dati JSON', details: error.message });
   }
 };
