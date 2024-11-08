e0d742aaa4d927cac79a104d8e40c34365b47f90
Sergio Cima <sergiocima@gmail.com>
2024-11-08 16:47:05 +0100

    apro articolo

--- scire-vue/src/main.js
+++ scire-vue/src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Importa il router
+import './style.css'; // Importa il file CSS globale
 
-createApp(App).mount('#app')
-
+// Crea l'app Vue, utilizza il router e monta l'applicazione
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
